37cb4246283a28273f298d0993f96c604aa36b4c
BigUncleYemi <olayemioyebanji911@gmail.com>
2019-03-17 22:42:52 +0100

    Updates

---
+++ precache-manifest.21e670d279e311f9d8e48f4da82a4d3a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4c75ef5a3ad472848d535e7a3175839",
+    "url": "/wils/static/media/amazon_PNG7@2x.d4c75ef5.png"
+  },
+  {
+    "revision": "705cdfa485c9899f61e5",
+    "url": "/wils/static/css/main.0c887465.chunk.css"
+  },
+  {
+    "revision": "83f13d67c3ca26c97e3b",
+    "url": "/wils/static/js/runtime~main.a77ff15e.js"
+  },
+  {
+    "revision": "87c7baaa9984ddec76731ac9a9897640",
+    "url": "/wils/static/media/eclipse_BG.87c7baaa.png"
+  },
+  {
+    "revision": "39cdcdc74da65ac9bce3",
+    "url": "/wils/static/js/2.24c56780.chunk.js"
+  },
+  {
+    "revision": "788cbd0c9eee877b3fe8d4eba332ebcf",
+    "url": "/wils/static/media/illustration.788cbd0c.png"
+  },
+  {
+    "revision": "0a1654d9e39991cadd05d5feb084b271",
+    "url": "/wils/static/media/illustration@2x.0a1654d9.png"
+  },
+  {
+    "revision": "a8ea2ef0e0c43d981187f16c8e8aaca3",
+    "url": "/wils/static/media/illustration@3x.a8ea2ef0.png"
+  },
+  {
+    "revision": "c0a95461b0f047277812503b0c45d09e",
+    "url": "/wils/static/media/Play_btn.c0a95461.svg"
+  },
+  {
+    "revision": "8f1cd9e7cb00faa1032be9e29d1caf28",
+    "url": "/wils/static/media/okreeka@3x.8f1cd9e7.png"
+  },
+  {
+    "revision": "c7c9a1ab89094b6b80242be03b710542",
+    "url": "/wils/static/media/amazon_PNG7.c7c9a1ab.png"
+  },
+  {
+    "revision": "705cdfa485c9899f61e5",
+    "url": "/wils/static/js/main.2805fd9b.chunk.js"
+  },
+  {
+    "revision": "80a2b8034ab265c2ea12fb4d5bfba676",
+    "url": "/wils/static/media/amazon_PNG7@3x.80a2b803.png"
+  },
+  {
+    "revision": "4ceceb6efb919ba68561b195119e85c2",
+    "url": "/wils/static/media/hnm@2x.4ceceb6e.png"
+  },
+  {
+    "revision": "f5d00d53779bfa7e1125ca6b8e94c55c",
+    "url": "/wils/static/media/hnm@3x.f5d00d53.png"
+  },
+  {
+    "revision": "16c191bf4578643102785e72cad08145",
+    "url": "/wils/static/media/Walmart_logo.svg@2x.16c191bf.png"
+  },
+  {
+    "revision": "ded9053cb0008b3a8a571364ca940b7b",
+    "url": "/wils/static/media/Walmart_logo.svg@3x.ded9053c.png"
+  },
+  {
+    "revision": "7b2941ae741a9997d5e522eab052bf0b",
+    "url": "/wils/static/media/Zalando_logo.svg@2x.7b2941ae.png"
+  },
+  {
+    "revision": "2840234fde91cd6725c89b1a44a07243",
+    "url": "/wils/static/media/Zalando_logo.svg@3x.2840234f.png"
+  },
+  {
+    "revision": "e69d62cdbefaa75ae753e5e9ed187129",
+    "url": "/wils/static/media/grey_BG.e69d62cd.svg"
+  },
+  {
+    "revision": "639356e698c1770fc467d65025c5f4d9",
+    "url": "/wils/static/media/BG_illustration.639356e6.png"
+  },
+  {
+    "revision": "39cdcdc74da65ac9bce3",
+    "url": "/wils/static/css/2.e471eeff.chunk.css"
+  },
+  {
+    "revision": "2514bf64df887acc4c31f58ce2541caf",
+    "url": "/wils/index.html"
+  }
+];
